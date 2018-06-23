2ce22e57aaa11f7bc5f0f8f21cc971f6a5a2e799
Nikita <razmakhnin9402@gmail.com>
2018-06-23 17:07:44 +0700

    Cleanup

--- src/DataProvider.hs
+++ src/DataProvider.hs
@@ -1,49 +1,11 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE GADTs #-}
 
 module DataProvider where
 
 import Data.Proxy
-
-type family MapDataProviders e where
-  MapDataProviders () = ()
-  MapDataProviders (a, b) = (MapDataProviders a, MapDataProviders b)
-  MapDataProviders (a, b, c) = ( MapDataProviders a
-                               , MapDataProviders b
-                               , MapDataProviders c)
-  MapDataProviders (a, b, c, d) = ( MapDataProviders a
-                                  , MapDataProviders b
-                                  , MapDataProviders c
-                                  , MapDataProviders d)
-  MapDataProviders (a, b, c, d, e) = ( MapDataProviders a
-                                     , MapDataProviders b
-                                     , MapDataProviders c
-                                     , MapDataProviders d
-                                     , MapDataProviders e)
-  MapDataProviders (a, b, c, d, e, h) = ( MapDataProviders a
-                                        , MapDataProviders b
-                                        , MapDataProviders c
-                                        , MapDataProviders d
-                                        , MapDataProviders e
-                                        , MapDataProviders h)
-  MapDataProviders (a, b, c, d, e, h, g) = ( MapDataProviders a
-                                           , MapDataProviders b
-                                           , MapDataProviders c
-                                           , MapDataProviders d
-                                           , MapDataProviders e
-                                           , MapDataProviders h
-                                           , MapDataProviders g)
-  MapDataProviders [e] = [DataProviderModel e]
-  MapDataProviders e = DataProviderModel e
 
 type family PackingResult model where
   PackingResult () = ()
